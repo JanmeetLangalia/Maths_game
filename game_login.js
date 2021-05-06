f6ebd2a2900a14e9aae9d9c2ddf77d62231ee04f
JanmeetLangalia <73023816+JanmeetLangalia@users.noreply.github.com>
2021-05-06 20:04:28 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,11 +1,16 @@
 
 function addUser(){
+
     player1_name=document.getElementById("player1_name_input").value;
     player2_name=document.getElementById("player2_name_input").value;
-
+ 
     localStorage.setItem("player1_name",player1_name);
     localStorage.setItem("player2_name",player2_name);
     
+
+    player1_name_stored=localStorage.getItem("player1_name");
+    console.log (player1_name_stored);
     window.location="game_page.html";
+    
 
 }
